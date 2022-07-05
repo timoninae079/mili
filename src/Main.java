e6654ca692d6d16712b075d320b14d154a65cd63
ЕленаПрекрасная <timoninae079@gmail.com>
2022-07-05 14:43:12 +0300

    мили

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 2000;
+        int priceMile = 20;
+
+        int bonus = price / priceMile;
+        System.out.println("количество бонусных миль-" + "" + bonus);
+    }
+}
